7b107e3817f419e45164ab3de4e0a27f27193a26
Bonnie Brown <bonnie.brown@jane.app>
2022-12-07 18:12:44 +0000

    added models and controllers

---
+++ bidder_api/db/migrate/20221207175655_create_auctions.rb
@@ -0,0 +1,11 @@
+class CreateAuctions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :auctions do |t|
+      t.string :title
+      t.text :description
+      t.date :end_time
+      t.integer :price
+      t.timestamps
+    end
+  end
+end
